1abea3b01b1c0150a392409021aaa2ab32e3c765
Javier Burroni <javier.burroni@gmail.com>
2018-04-03 10:28:56 -0400

    changes to support new way to build random variables

--- test/tests/mm.jl
+++ test/tests/mm.jl
@@ -15,17 +15,17 @@ y_obs = vcat((randn(div(nobs, 2)) + 50)-10, (randn(div(nobs,2))))
 
 λ = normal(μ_data, sqrt(σ²data))
 
-r = Γ(1, 1/σ²data)
+r = Γ(1.0, 1/σ²data)
 
 # FIXME: overload Base.vect
 μ =  [normal(λ, 1/r) for i = 1:k]
 
 # Inference goal: conditional posterior distribution of means given data
-β = inversegamma(1, 1)
-w = Γ(1, σ²data)
+β = inversegamma(1.0, 1.0)
+w = Γ(1.0, σ²data)
 s = [Γ(β, 1/w) for i = 1:k]
 
-α = Γ(1, 1)
+α = Γ(1.0, 1.0)
 a_k = α / k
 
 π = dirichlet([a_k for i = 1:k])
@@ -35,8 +35,8 @@ mm(π, μ, s) = sum([π[i] * normal(μ[i], s[i]) for i = 1:k])
 
 y = [mm(π, μ, s) for _ in y_obs]
 
-y_ = Mu.randvec(y)
+y_ = Mu.randarray(y)
 
 # Inference goal: conditional distribution of means given data
-samples = rand(Mu.randvec(μ), y_ == y_obs, alg=MH, n=10000)
+samples = rand(Mu.randarray(μ), y_ == y_obs, alg=MH, n=10000)
 @show [median(map(x->x[i], samples)) for i=1:k]
