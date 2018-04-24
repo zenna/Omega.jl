d6c89d98c58d542327b26ed2f3e85195acfada8a
Javier Burroni <javier.burroni@gmail.com>
2018-04-24 15:07:04 -0400

    rand to generate equality

--- test/models/autoencoder.jl
+++ test/models/autoencoder.jl
@@ -1,41 +1,13 @@
 using PyTorch
 using PyCall
+using ProgressMeter
+using UnicodePlots
 
 
 # from https://github.com/L1aoXingyu/pytorch-beginner/blob/master/08-AutoEncoder/conv_autoencoder.py
 
 
 variable(x::Array) = PyTorch.autograd.Variable(PyTorch.torch.Tensor(x))
-
-@pydef type Autoencoder <: nn.Module
-  __init__(self) = begin
-      pybuiltin(:super)(Autoencoder, self)[:__init__]()
-      self[:encoder] = nn.Sequential(
-          nn.Conv2d(3, 16, 3, stride=3, padding=1),  # b, 16, 10, 10 #output = 34x34x16
-          nn.ReLU(true),
-          #nn.MaxPool2d(2, stride=2, return_indices=true),  # b, 16, 5, 5 # 18x18x16
-          nn.Conv2d(16, 1, 2, stride=2, padding=1),  # b, 8, 3, 3  18x18x1
-          nn.ReLU(true),
-          #nn.MaxPool2d(2, stride=1, return_indices=true)  # b, 8, 2, 2  # 8x8x1
-      )
-      self[:decoder] = nn.Sequential(
-          #nn.MaxUnpool2d(2, stride=1), # 9x9x1
-          nn.ConvTranspose2d(1, 16, 2, stride=2, padding=1),  # b, 16, 5, 5 
-          nn.ReLU(true),
-          #nn.MaxUnpool2d(2, stride=2), # 9x9x1
-          nn.ConvTranspose2d(16, 3, 3, stride=3, padding=1),  # b, 8, 15, 15
-          #nn.ReLU(true),
-          #nn.ConvTranspose2d(8, 3, 2, stride=2, padding=1),  # b, 1, 28, 28
-          nn.Sigmoid()
-      )
-    end
-
-  forward(self, x) = begin
-        x = self[:encoder](x)
-        x = self[:decoder](x)
-    end
-  end
-
 
 @pydef type Autoencoder <: nn.Module
   __init__(self) = begin
@@ -81,24 +53,20 @@ end
 optimizer = optim.Adam(model[:parameters](), lr=0.001,
                              weight_decay=1e-5)
 
-function example_spheres()
-  RayTrace.ListScene(
-    [FancySphere(Vec3([0.0,      0, -20]),     4.0, Vec3([1.00, 0.32, 0.36]), 1.0, 0.0, Vec3([0.0, 0.0, 0.0])),
-    FancySphere(Vec3([5.0,     -1, -15]),     2.0, Vec3([0.90, 0.76, 0.46]), 1.0, 0.0, Vec3([0.0, 0.0, 0.0])),
-    FancySphere(Vec3([5.0,      0, -25]),     3.0, Vec3([0.65, 0.77, 0.97]), 1.0, 0.0, Vec3([0.0, 0.0, 0.0])),
-    FancySphere(Vec3([-5.5,      0, -15]),    3.0, Vec3([0.90, 0.90, 0.90]), 1.0, 0.0, Vec3([0.0, 0.0, 0.0])),
-    # light (emission > 0)
-    FancySphere(Vec3([0.0,     20.0, -30]),  3.0, Vec3([0.00, 0.00, 0.00]), 0.0, 0.0, Vec3([3.0, 3.0, 3.0]))])
+function to_batched(data::Vector)
+  imgs = permutedims(cat(4, data...), [4, 3, 1, 2]);
+  imgs/255.0
 end
 
-img_obs = render(example_spheres());
+function to_torch(data::Vector)
+  data |> to_batched |> variable
+end
 
 N = 1000-1
 
 imgs = [rand(img) for i in 1:N];
 push!(imgs, img_obs);
-imgs = permutedims(cat(4, imgs...), [4, 3, 1, 2]);
-imgs = imgs/255.0;
+imgs_batched = imgs |> to_batched;
 batch_size = 40;
 num_epochs = 200;
 
@@ -106,7 +74,7 @@ for epoch in 1:num_epochs
   permutation = randperm(size(imgs)[1]);
   l = 0.0
   for part in Iterators.partition(permutation, batch_size)
-    im = variable(imgs[part, :, :, :]);
+    im = variable(imgs_batched[part, :, :, :]);
     output = model(im);
     loss = criterion2(output, im)
     optimizer[:zero_grad]();
@@ -117,17 +85,68 @@ for epoch in 1:num_epochs
   println("epoch $epoch/$num_epochs, loss:$(l)");
 end
 
-function to_batched(data::Vector)
-  imgs = permutedims(cat(4, data...), [4, 3, 1, 2]);
-  imgs = imgs/255.0
+# prediction = model(variable(imgs))
+# prediction = prediction[:data][:numpy]();
+# prediction = prediction*255;
+# prediction2 = permutedims(prediction, [1, 3, 4, 2]);
+# imshow(rgbimg(prediction2[1, :,:,:]))
+
+
+
+
+"Sample from `x | y == true` with Single Site Metropolis Hasting"
+function Base.rand(x::Mu.RandVar{T}, target_img,
+                   encoder;
+                   n::Integer = 1000,
+                   OmegaT::OT = Mu.DefaultOmega) where {T, OT}
+  ω = OmegaT()
+  target = encoder(target_img)
+  distance(x) = -((x - target) .^2 |> sum)
+  last =  ω |> x |>  encoder |> distance
+  qlast = 1.0
+  samples = []
+  accepted = 0.0
+  @showprogress 1 "Running Chain" for i = 1:n
+    ω_ = if isempty(ω)
+      ω
+    else
+      Mu.update_random(ω)
+    end
+    p_ = ω_ |> x |>  encoder |> distance
+    ratio = p_ - last
+    if (rand() |> log) < ratio
+      ω = ω_
+      plast = p_
+      accepted += 1.0
+    end
+    push!(samples, ω)
+  end
+  print_with_color(:light_blue, "acceptance ratio: $(accepted/float(n))\n")
+  samples
 end
 
-function to_torch(data::Vector)
-  data |> to_torch |> variable
-end
 
-prediction = model(variable(imgs))
-prediction = prediction[:data][:numpy]();
-prediction = prediction*255;
-prediction2 = permutedims(prediction, [1, 3, 4, 2]);
-imshow(rgbimg(prediction2[1, :,:,:]))
+samples = rand(img, 
+                img_obs,
+                (x) -> model[:encoder]([x,] |>to_torch)[:data][:numpy](),
+                n=40000);
+
+
+
+
+
+
+# random projection                
+rand_proj_mat = randn(50, 100*100*3);
+
+
+encoder(x) = rand_proj_mat * reshape(x, 100*100*3, 1)
+
+samples = rand(img, 
+                img_obs,
+                encoder,
+                n=40000);
+
+z_obs = encoder(img_obs)
+
+distances = [(z_obs - encoder(img(rng))).^2 |> sum for rng in samples];
