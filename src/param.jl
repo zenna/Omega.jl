7e7e2e37c6b3e7e3de463b47dc4ad81b007cb9e8
Zenna Tavares <zennatavares@gmail.com>
2018-04-27 12:16:01 -0400

    sample from params

--- src/param.jl
+++ src/param.jl
@@ -5,26 +5,34 @@ end
 
 Params() = Params{Symbol, Any}(Dict{Symbol, Any}())
 Base.getindex(θ::Params{I}, i::I) where I = θ.d[i]
+"Set default value"
+Base.getindex(θ::Params{I}, i::I, v) where I = θ.d[i] = v
 Base.setindex!(θ::Params{I}, v, i::I) where I = θ.d[i] = v 
 
-function test()
-  p = Params()
-  p[:x] = 2x+1 + p[y]
+function mem(ω::Omega, ::Params)
 end
 
 ## TODO: Saving / loading.  Use BSON!
 
-function save(ω::Omega)
-end
-
-function testparam()
-  θ = Params()
-  θ[:x] = 5.0
-  θ[:y] = uniform(0.0, 1.0) + θ[:x]
-end
+save(ω::Omega, fname) = bson(fname, ω)
 
 
+"Params sample. Any `val` which is a `RandVar` in `φ` are sampled"
+function Base.rand(ω::Omega, φ::Params)
+  Params(Dict(k => apl(v, ω) for (k, v) in φ.d))
+end 
+
+Base.rand(φ::Params) = rand(DefaultOmega(), φ)
 
 # TODO: Stable IDs from Omega
 # Problem 1.  [@id] is global, but it need only be relative (maybe?!)
 # Problem 2. randvar ids are global
+
+"RandVar which has randomness memoized"
+struct MemoizedRandVar{O, X}
+  ω::O
+  x::X
+end
+
+Base.show(io::IO, φ::Params) = show(io, φ.d)
+Base.display(φ::Params) = (println("Params"); display(φ.d))
