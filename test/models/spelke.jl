25706ca057b968e9430c9e1ef70dd1b5ebbfdfad
Zenna Tavares <zennatavares@gmail.com>
2018-04-30 21:18:36 -0400

    objects

--- test/models/spelke.jl
+++ test/models/spelke.jl
@@ -41,9 +41,10 @@ function draw(obj, canvas, color = :blue)
   corners_ = corners(obj)
   for i = 1:length(corners_)
     p1 = corners_[i]
-    p2 = i < length(corners_) ? corners_[i + 1] : corners_[i]
+    p2 = i < length(corners_) ? corners_[i + 1] : corners_[1]
     lines!(canvas, p1..., p2..., color)
   end
+  canvas
 end
 
 "Draw Scene"
@@ -51,6 +52,7 @@ function draw(scene::Scene,
               canvas = BrailleCanvas(100, 100, origin_x = 0., origin_y = 0., width = 100., height = 100.))
   draw(scene.camera, canvas, :red)
   foreach(obj -> draw(obj, canvas, :blue), scene.objects)
+  canvas
 end
 
 nboxes = poisson(5)
@@ -58,10 +60,10 @@ nboxes = poisson(5)
 "Scene at frame t=0"
 function initscene(ω)
   objects = map(1:nboxes(ω)) do i
-    Object(uniform(ω[@id], 0.0, 1.0),
-           uniform(ω[@id], 0.0, 1.0),
-           uniform(ω[@id], 0.0, 1.0),
-           uniform(ω[@id], 0.0, 1.0))
+    Object(uniform(ω[@id][i], 0.0, 1.0),
+           uniform(ω[@id][i], 0.0, 1.0),
+           uniform(ω[@id][i], 0.0, 30.0),
+           uniform(ω[@id][i], 0.0, 30.0))
   end
   camera = Camera(uniform(ω[@id], 0.0, 1.0),
                   uniform(ω[@id], 0.0, 1.0),
@@ -79,14 +81,14 @@ end
 
 function move(ω, scene::Scene)
   @grab scene
-  Scene(map(obj -> move(ω, obj), scene.objects), scene.camera)
+  Scene(map(iobj -> move(ω[iobj[1]], iobj[2]), enumerate(scene.objects)), scene.camera)
 end
 
 function video_(ω, nsteps = 1000)
   scene = initscene(ω)
   trajectories = Scene[]
   for i = 1:nsteps
-    scene = move(ω, scene)
+    scene = move(ω[i], scene)
     push!(trajectories, scene)
   end
   trajectories
@@ -94,3 +96,7 @@ end
 
 video = iid(video_)
 rand(video)
+
+# TODO
+# Observe data
+# Need distance function on Scene
