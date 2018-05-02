ac5c7d970c7f88fb56621d26b5d0b9f9919a16b0
minasyan <minasyan@mit.edu>
2018-05-02 17:17:16 -0400

    complete implementation of mnist on sghmc, NaN bug when taking gradients

--- src/inference/sghmc.jl
+++ src/inference/sghmc.jl
@@ -6,7 +6,7 @@ notunit(ω) = ω > 1.0 || ω < 0.0
 
 "Stochastic Gradient Hamiltonian Monte Carlo with Langevin Dynamics Friction: https://arxiv.org/pdf/1402.4102.pdf"
 function sghmc(ygen, nsteps, stepsize, current_q::Vector, ω, state)
-  d = length(q)
+  d = length(current_q)
   q = current_q
   p = randn(d)
   current_p = p
@@ -15,17 +15,23 @@ function sghmc(ygen, nsteps, stepsize, current_q::Vector, ω, state)
   any(notunit, q) && return (current_q, false, state)
 
   # construct friction and noise estimate matrices
-  Bhat = zeros(d, d); C = eye(d)
+  Bhat = 0.1
+  C = 1
 
   for i = 1:nsteps
+    #@show i
     # generate a predicate and get the gradient of its ϵ
     predicate, state = ygen(state)
     ∇U(q) = gradient(predicate, unlinearize(q, ω), q)
 
     # update the location and momentum parameters
+    #@show q[1], p[1]
     q = q .+ stepsize .* p
+    #@show q[1], p[1]
     any(notunit, q) && return (current_q, false, state)
-    p = p - stepsize .* ∇U(q) - stepsize .* C * p + rand(MvNormal(2 * stepsize .* (C .- Bhat)))
+    #@show state
+    p = p - stepsize .* ∇U(q) - stepsize .* C * p + rand(MvNormal(d, 2 * stepsize .* (C .- Bhat)))
+    #@show ∇U(q)
   end
 
   # no MH step necessary
@@ -36,7 +42,7 @@ end
 function Base.rand(OmegaT::OT, ygen, alg::Type{SGHMC}, state;
                    n=1000,
                    nsteps = 100,
-                   stepsize = 0.0001) where {T, OT}
+                   stepsize = 0.000001) where {OT}
   ω = OmegaT()
   predicate, state = ygen(state)
   predicate(ω) # Initialize omega
@@ -52,11 +58,12 @@ function Base.rand(OmegaT::OT, ygen, alg::Type{SGHMC}, state;
     end
     i % 10 == 0 && print_with_color(:light_blue,  "acceptance ratio: $(accepted/float(i))\n")
   end
+  #@grab ωsamples
   ωsamples
 end
 
 "Sample from `x | y == true` with Stochastic Gradient Hamiltonian Monte Carlo"
 function Base.rand(x::RandVar{T}, ygen, alg::Type{SGHMC}, state;
-                   n::Integer = 1000, OmegaT::OT = DefaultOmega) where {T, OT}
+                   n::Integer = 1000, OmegaT::OT = Mu.SimpleOmega{Int, Float64}) where {T, OT}
   map(x, rand(OmegaT, ygen, alg, state, n=n))
 end
--- test/models/mnist.jl
+++ test/models/mnist.jl
@@ -6,7 +6,7 @@ const batch_size = 128
 Mu.lift(:(Flux.σ), 1)
 Mu.lift(:(Flux.softmax), 1)
 
-function σ3(x::Array)
+function σ3(x)
   ones(x) ./ (ones(x) + exp.(-x))
 end
 Mu.lift(:σ3, 1)
@@ -20,22 +20,22 @@ end
 "Bayesian Multi Layer Percetron"
 function mlp()
   nin = MNIST.NROWS * MNIST.NCOLS
-  nout = 100
+  # nout = 100
   # FIXME: This should be normal(0.0, 1.0, (nin, nout)
-  w1 = randarray([normal(0.0, 1.0) for i = 1:nin, j = 1:nout])
-  nin = 100
-  nout = 100
-  w2 = randarray([normal(0.0, 1.0) for i = 1:nout, j = 1:nin])
-  nin = 100
+  # w1 = randarray([normal(0.0, 1.0) for i = 1:nin, j = 1:nout])
+  # nin = 100
+  # nout = 20
+  # w2 = randarray([normal(0.0, 1.0) for i = 1:nin, j = 1:nout])
+  # nin = 20
   nout = 10
-  w3 = randarray([normal(0.0, 1.0) for i = 1:nout, j = 1:nin])
-  function f(x)
-    a = σ3(w1 * x)
-    b = σ3(w2 * a)
-    c = σ3(w3 * b)
+  w3 = randarray([normal(0.0, 1.0) for i = 1:nin, j = 1:nout])
+  function f(x; weight3=w3)
+    # a = σ3(x * weight1)
+    # b = σ3(a * weight2)
+    c = σ3(x * weight3)
     Flux.softmax(c)
   end
-  f, w1, w2, w3
+  f, w3
 end
 
 "Infinite iterator over MNIST"
@@ -47,24 +47,38 @@ function mnistcycle(batch_size)
   Iterators.zip(batchgen_x, batchgen_y)
 end
 
-function ygengen()
-  xyiter = mnistcycle()
-  state = start(xyiter)
-  function ygen()
-    item, state = next(xyiter, state)
-    return item
-  end
-end
-
 "Train MNIST using Stochastic Gradient HMC"
 function train(niter)
-  f, w1, w2, w3 = mlp()
+  f, w3 = mlp()
   gen = mnistcycle(batch_size)
   state = start(gen)
   function ygen(state)
     item, state = next(gen, state)
-    predicate = f(item[1]) == item[2]
+    batch_x = transpose(item[1])
+    batch_y = convert(Array{Int64}, Flux.onehotbatch(convert(Array{Int64}, item[2]), 0:9))
+    predicate = f(batch_x) == batch_y
     return predicate, state
   end
-  samples = rand(randarray([w1, w2, w3]), ygen, Mu.SGHMC, state, n=niter)
+  samples = rand(w3, ygen, Mu.SGHMC, state, n=niter)
+  #@grab samples
+end
+
+"Test Bayesian network for MNIST using SGHMC"
+function test(niter)
+  f, _ = mlp()
+  weights = mean(train(niter))
+  test_x, test_y = MNIST.testdata()
+  correct = 0
+  #@grab weights
+  for i = 1:size(test_y)[1]
+    x = transpose(test_x[:, i])
+    onehot_y = f(x, weight3=weights)
+    #@grab onehot_y
+    y = Flux.argmax(transpose(onehot_y))[1]
+    testy = convert(Int64, test_y[i])
+    if y == testy
+      correct += 1
+    end
+  end
+  accepted = correct / size(test_y)[1]
 end
