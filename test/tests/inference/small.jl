eb022b22fdb396a4ae84f58936ebd031d5bf10e1
Zenna Tavares <zennatavares@gmail.com>
2018-05-09 14:05:57 -0400

    learn

--- test/tests/inference/small.jl
+++ test/tests/inference/small.jl
@@ -31,26 +31,6 @@ function simple2(ndim = ALG)
   samples = rand(x, sum(x) = 1.0, ALG)
 end
 
-"Test equality of random variables"
-function simpleeq(ALG)
-  x = normal(0.0, 1.0)
-  y = normal(0.0, 1.0)
-  diff = abs(x - y)
-  β = kumaraswamy(0.1, 5.0)
-  k = Mu.kf1β(β)
-  # α = uniform(0.0, 5.0)
-  # α = 3.0
-  # k = Mu.kseα(α)
-  n = 5000000
-  OmegaT = SimpleOmega{Int, Float64}
-  samples = rand(OmegaT, ≊(x, y, k), ALG;
-                 n = n,
-                 cb = [Mu.default_cbs(n);
-                       throttle(Mu.plotrv(β, "Temperature: β"), 1);
-                       throttle(Mu.plotω(x, y), 1);
-                       throttle(Mu.plotrv(diff, "||x - y||"), 1)])
-end
-
 for ALG in Mu.Algorithm
   println("Testing $ALG")
   simple1(ALG)
--- test/tests/inference/tempering.jl
+++ test/tests/inference/tempering.jl
@@ -1,6 +1,44 @@
 using Mu
 using UnicodePlots
 
+"Test equality of random variables"
+function simpleeq(ALG)
+  x = normal(0.0, 1.0)
+  y = normal(0.0, 1.0)
+  diff = abs(x - y)
+  β = kumaraswamy(0.1, 5.0)
+  k = Mu.kf1β(β)
+  # α = uniform(0.0, 5.0)
+  # α = 3.0
+  # k = Mu.kseα(α)
+  n = 5000000
+  OmegaT = SimpleOmega{Int, Float64}
+  samples = rand(OmegaT, ≊(x, y, k), ALG;
+                 n = n,
+                 cb = [Mu.default_cbs(n);
+                       throttle(Mu.plotrv(β, "Temperature: β"), 1);
+                       throttle(Mu.plotω(x, y), 1);
+                       throttle(Mu.plotrv(diff, "||x - y||"), 1)])
+end
+
+"Test equality of random variables"
+function simpleeq(ALG)
+  x = normal(0.0, 1.0)
+  y = normal(0.0, 1.0)
+  diff = abs(x - y)
+  β = kumaraswamy(0.1, 5.0)
+  k = Mu.d(x, y)
+  n = 5000000
+  OmegaT = SimpleOmega{Int, Float64}
+  samples = rand(OmegaT, ≊(x, y, k), ALG;
+                 n = n,
+                 cb = [Mu.default_cbs(n);
+                       throttle(Mu.plotrv(β, "Temperature: β"), 1);
+                       throttle(Mu.plotω(x, y), 1);
+                       throttle(Mu.plotrv(diff, "||x - y||"), 1)])
+end
+
+
 μ = normal(0.0, 1.0)
 x = normal(μ, 1.0)
 α = betarv(1.0, 2.0)
