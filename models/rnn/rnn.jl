848577975fa31d31972fbf96dc831280792bdebc
Zenna Tavares <zennatavares@gmail.com>
2018-05-13 23:51:53 -0400

    rnn

--- models/rnn/rnn.jl
+++ models/rnn/rnn.jl
@@ -4,21 +4,11 @@ using UnicodePlots
 using DataFrames
 using CSV
 
-data = CSV.read(joinpath(ENV["DATADIR"], "mu", "glucosedata.csv"))
-## Model
-## =====
 Mu.defaultomega() = SimpleOmega{Vector{Int}, Array}
 
-# Constraint
 δ = 0.1
 d(x, y) = (x - y)^2
 Mu.lift(:d, 2)
-
-npatients = 5
-F_(ω, i) = Flux.Dense(ω[@id][i], 5, 1, Flux.elu)
-
-# Create one network per person
-fs = [iid(F_, i) for i = 1:npatients]
 
 "Recurrent Neural Network"
 function rnn_(ω, f, nsteps) 
@@ -31,16 +21,28 @@ function rnn_(ω, f, nsteps)
   [xs...]
 end
 
-# Create one simulation RandVar for each patient
-nsteps = 20
-sims = [iid(rnn_, f, nsteps) for f in fs]
+function model(nsteps)
+  npatients = 5
+  F_(ω, i) = Flux.Dense(ω[@id][i], 5, 1, Flux.elu)
 
-# Take average over time
-meansims = mean.(sims)
+  # Create one network per person
+  fs = [iid(F_, i) for i = 1:npatients]
 
-## Without RCD
-ties = [d(meansims[i], meansims[j]) < δ for i = 1:npatients, j = 1:npatients if i != j]
-simulations = rand((sims...), (&)(ties...); OmegaT = Mu.defaultomega())
+  # Create one simulation RandVar for each patient
+  sims = [iid(rnn_, f, nsteps) for f in fs]
+
+  # Take average over time
+  meansims = mean.(sims)
+  sims, meansims
+end
+
+# function ok()
+#   ties = [d(meansims[i], meansims[j]) < δ for i = 1:npatients, j = 1:npatients if i != j]
+#   simulations = rand((sims...), (&)(ties...); OmegaT = Mu.defaultomega())
+
+#   d1, obvglucose = datacond2(data, sims[1], 3)
+#   simsω = rand(Mu.defaultomega, d1, HMC, n=1000000);
+# end
 
 function traces(data, i, measure = 807)
   people = groupby(data, :Id)
@@ -49,23 +51,33 @@ function traces(data, i, measure = 807)
   sort(p2, :Time)
 end
 
-"wow!"
-function datacond(data, sim, personid)
+"Data condition, returns (sim == peronid.data, peronid.data)"
+function datacond(data, sim, personid, nsteps)
   exampledata = traces(data, personid)
-  rng = 1:min(nsteps, nrow(exampledata))
-  obvglucose = normalize(Float64.(exampledata[:Value])[rng])
-  datacond = sim[rng] == obvglucose
+  range = 1:min(nsteps, nrow(exampledata))
+  obvglucose = normalize(Float64.(exampledata[:Value])[range])
+  datacond = sim[range] == obvglucose
   datacond, obvglucose
 end
 
-d1, obvglucose = datacond2(data, sims[1], 3)
-simsω = rand(Mu.defaultomega, d1, HMC, n=1000000);
+"Load the data"
+loaddata() = CSV.read(joinpath(ENV["DATADIR"], "mu", "glucosedata.csv"))
+
+"Data, model, condition"
+function infer(nsteps = 20)
+  data = loaddata()
+  sims, meansims = model(nsteps)
+  personid = 3
+  y, obvglucose = datacond(data, sims[1], personid, nsteps)
+  simsω = rand(SimpleOmega{Vector{Int}, Flux.Array}, y, HMC, n=10000)
+  simsω, obvglucose, sims
+end
 
 ## Plots
 ## ====
 using Plots
 "n simulations, n + 1 simulations, with mean tied"
-function plot1(sims, dpi = 80)
+function plot1(sims, dpi = 80, save = false)
   p = Plots.plot(sims, w=3,
                  title = "Time vs Glucose Level",
                  xaxis = "Time",
@@ -73,7 +85,7 @@ function plot1(sims, dpi = 80)
                  fmt = :pdf,
                  size = (Int(5.5*dpi), 2*dpi),
                  dpi = dpi)
-  savefig(p, joinpath(ENV["DATADIR"], "mu", "figures", "test.pdf"))
+  save && savefig(p, joinpath(ENV["DATADIR"], "mu", "figures", "test.pdf"))
   p
 end
 
@@ -88,5 +100,7 @@ function setupplots()
   default(dpi = 300) #Only for PyPlot - presently broken
 end
 
-traces(data, 3)[:Value]
-plot1([sims[1](simsω[end]), obvglucose])
+function main()
+  simsω, obvglucose, sims = infer()
+  plot1([sims[1](simsω[end]), obvglucose])
+end
