fee227d8b70797f84f829569d1d8558102b51b36
Xin Zhang <xinzhangsjtu1989@gmail.com>
2018-07-02 23:48:58 -0400

    fairness example from fairsquare. Currently only conditionally on the fairness condition. Accuracy is ignored. The parameters are taken from fairsquare and gaussian noise is added.

--- test/benchmarks/fairness.jl
+++ test/benchmarks/fairness.jl
@@ -1,67 +1,84 @@
+using Revise
 using Omega
 
-"A Person"
-struct Person
-  ismale::Float64
-  height::Float64
-  age::Float64
-  isrich::Float64
+function popModel_(ω, sex)
+    # sex = step_dis_(ω, [(0,1,0.3307), (1,2,0.6693)])
+    # sex = Omega.categorical(ω, [0.3307, 0.6693])
+    # sex -= 0.5
+    if sex < 1
+        capital_gain = normal(ω, 568.4105, 24248365.5428)
+        if capital_gain < 7298.0000
+            age = normal(ω, 38.4208, 184.9151)
+            education_num = normal(ω, 10.0827, 6.5096)
+            capital_loss = normal(ω, 86.5949, 157731.9553)
+        else
+            age = normal(ω, 38.8125, 193.4918)
+            education_num = normal(ω, 10.1041, 6.1522)
+            capital_loss = normal(ω, 117.8083, 252612.0300)
+        end
+    else
+        capital_gain = normal(ω, 1329.3700, 69327473.1006)
+        if capital_gain < 5178.0000
+            age = normal(ω, 38.6361, 187.2435)
+            education_num = normal(ω, 10.0817, 6.4841)
+            capital_loss = normal(ω, 87.0152, 161032.4157)
+        else
+            age = normal(ω, 38.2668, 187.2747)
+            education_num = normal(ω, 10.0974, 7.1793)
+            capital_loss = normal(ω, 101.7672, 189798.1926)
+        end
+    end
+
+    if (education_num > age)
+        age = education_num
+    end
+    # sensitiveAttribute(sex < 1)
+    # qualified(age > 18)
+    return (sex, age, capital_gain, capital_loss)
 end
 
-ismale(p::Person) = p.ismale
-height(p::Person) = p.height
-age(p::Person) = p.age
-isrich(p::Person) = p.isrich
-σ(x) = one(x) / (one(x) + exp(-x))
-
-# Lifts (TODO: Automate this)
-# =====
-lift(:ismale, 1)
-lift(:height, 1)
-lift(:age, 1)
-lift(:isrich, 1)
-Omega.lift(:Person, 4)
-Omega.lift(:σ, 1)
-
-
-person = Person(bernoulli(0.3),
-                uniform(130.0, 180.0),
-                uniform(18.0, 50.0),
-                bernoulli(0.3))
-
-woman = Person(0.0,
-               uniform(130.0, 180.0),
-               uniform(18.0, 50.0),
-               bernoulli(0.3))
-
-man = Person(1.0,
-             uniform(130.0, 180.0),
-             uniform(18.0, 50.0),
-             bernoulli(0.3))
-
-
-θ = [normal(0.0, 1.0) for i = 1:3]
-θ_ = randarray(θ)
-
-# Problem is is 
-# "Linear Classifier"
-function isrich(w, person::Person)
-  σ(person(w).height * θ[1](w) + person(w).age * θ[2](w) + person(w).ismale * θ[3](w))
-end
-# Omega.lift(:isrich, 1)
-
-## Classifier
-## ==========
-"Linear Classifier"
-function isrich2(person)
-  σ(height(person) * θ[1] + age(person) * θ[2] + ismale(person) * θ[3])
+function popModel(ω)
+    sex = Omega.categorical(ω, [0.3307, 0.6693])
+    sex -= 0.5
+    return popModel_(ω,sex)
 end
 
-ndata = 10
-xdata = [rand(person) for i = 1:ndata]
-ydata = [rand([0.0, 1.0]) for i in xdata]
+function maleModel(ω)
+    return popModel_(ω,1.5)
+end
 
-fairness = prob(isrich2(man) ∥ θ_) / prob(isrich(woman) ∥ θ_) < ϵ
-datacond = randarray([isrich2(xdata[i]) * bernoulli(0.9) for i = 1:length(xdata)])
+function femaleModel(ω)
+    return popModel_(ω,0.5)
+end
 
-samples = rand(θ_, (fairness > 0) & (datacond == ydata))
+W = randarray([normal(0.0006,1.0), normal(-5.7363,1.0), normal(-0.0002,1.0)])
+b = normal(1.0003,1.0)
+δ = normal(-0.0003,1.0)
+
+function F(ω, sex, age, capital_gain, capital_loss)
+    N_age = (age - 17.0) / 62.0
+    N_capital_gain = (capital_gain - 0.0) / 22040.0
+    N_capital_loss = (capital_loss - 0.0) / 1258.0
+    t = W[1](ω) * N_age + W[2](ω) * N_capital_gain + W[3](ω) * N_capital_loss + b(ω)
+    if sex > 1.0
+        t = t + δ(ω)
+    end
+    return t < 0
+    # fairnessTarget(t < 0)
+end
+
+t(ω) = F(ω, popModel(ω)[1], popModel(ω)[2],popModel(ω)[3],popModel(ω)[4])
+
+rand(iid(popModel))
+
+rand(iid(t))
+
+m_isrich_(ω) = F(ω, maleModel(ω)[1], maleModel(ω)[2], maleModel(ω)[3], maleModel(ω)[4])
+f_isrich_(ω) = F(ω, femaleModel(ω)[1], femaleModel(ω)[2], femaleModel(ω)[3], femaleModel(ω)[4])
+
+m_isrich = iid(m_isrich_; T = Bool)
+f_isrich = iid(f_isrich_; T = Bool)
+
+fairness = prob(f_isrich ∥ (W,b,δ), 100) / prob(m_isrich ∥ (W,b,δ), 100) > 0.85
+
+W_samples = rand(W, fairness)
