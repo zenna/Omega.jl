575bc67eb1e2526ba60d2cd9217b41f06a542f90
Zenna Tavares <zennatavares@gmail.com>
2018-08-12 06:52:02 -0400

    add urandvar

---
+++ src/randvar/urandvar.jl
@@ -0,0 +1,17 @@
+"Random Variable of Unknown distribution `ω -> f(ω[id], args...)`"
+struct URandVar{T, F, TPL <: Tuple} <: RandVar{T}
+  f::F
+  args::TPL
+  id::ID
+  URandVar{T}(f::F, args::TPL, id = uid()) where {T, F, TPL} =
+    new{T, F, TPL}(f, args, id)
+end
+
+"Parameters of the Random Variable"
+params(x::URandVar) = x.args
+
+"Function of the Random Variable"
+func(x::URandVar) = x.f
+
+"Name of a random variable"
+name(rv::URandVar) = string(rv.f)
