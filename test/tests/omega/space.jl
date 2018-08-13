c5998df913b5d8a9f32fc0c2f337305ae641045b
James Koppel <jamesbkoppel@yahoo.com>
2018-08-13 16:50:07 -0400

    Commenting test

--- test/tests/omega/space.jl
+++ test/tests/omega/space.jl
@@ -16,11 +16,13 @@ using Omega.Space
 
   @assert length(vec1) == length(vec2)
 
+  # Test sequential
   for i in 2:length(vec1)
     @test vec1[i] == vec1[i-1] + 1
     @test vec2[i] == vec2[i-1] + 1
   end
 
+  # Test different ID generators are disjoint
   @test isempty(intersect(vec1, vec2))
 end
 
