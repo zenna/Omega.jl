1706ac1783a568cb8c6e1bd359982ff36c6e2f68
James Koppel <jamesbkoppel@yahoo.com>
2018-08-13 00:40:35 -0400

    Adding first tests for Omega.Index. I think I broke some existing tests though

---
+++ test/tests/omega/index.jl
@@ -0,0 +1,16 @@
+module Test
+
+using Base.Test
+
+using Omega.Index
+
+@testset "Increment" begin
+  x = [1,2,3]
+  increment!(x)
+  @test x == [1,2,4]
+
+  @test increment(x) == [1,2,5]
+  @test x == [1,2,4]
+end
+
+end
