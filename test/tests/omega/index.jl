39ea43ff3839f4ab5b8f3dddb05734998876bb11
James Koppel <jamesbkoppel@yahoo.com>
2018-08-13 16:23:28 -0400

    Testing the combine function

--- test/tests/omega/index.jl
+++ test/tests/omega/index.jl
@@ -13,4 +13,22 @@ using Omega.Index
   @test x == [1,2,4]
 end
 
+@testset "combine" begin
+  seen = Set()
+  vals = 0:10
+  for i in vals
+    for j in vals
+      p = pair(i, j)
+      @test !(p in seen)
+      push!(seen, p)
+    end
+  end
+
+  #TODO: What is the property needed in general for this function?
+  #The property tested here for the int version is too strong and does not hold.
+end
+
+# I still don't understand a spec for base outside of the definitions -- jkoppel
+# No tests for Paired/pair, because they should not be exported
+
 end
