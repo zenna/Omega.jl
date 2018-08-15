1ed65a2bfa50594417de4c8a53766e43b1dd33eb
James Koppel <jamesbkoppel@yahoo.com>
2018-08-15 00:02:33 -0400

    More index tests

--- test/tests/omega/index.jl
+++ test/tests/omega/index.jl
@@ -13,22 +13,39 @@ using Omega.Index
   @test x == [1,2,4]
 end
 
+# Vector indices should be a free semigroup
+#
+# Properties:
+# combine(a, combine(b, c)) === combine(combine(a, b), c)
+# (combine(a, b) == combine(a, c)) => b == c
+# (combine(a, c) == combine(b, c)) => a == b
+# append(a, b) === combine(a, base(b))
 @testset "combine" begin
-  seen = Set()
-  vals = 0:10
-  for i in vals
-    for j in vals
-      p = pair(i, j)
-      @test !(p in seen)
-      push!(seen, p)
+  test_elts = [ [1,2], [3], [3, 4]
+              , [5, 6], [7], [1,2,3]
+              ]
+  test_indivs = collect(1:10)
+
+  triples = [(a,b,c) for a in test_elts, b in test_elts, c in test_elts]
+
+  for (a,b,c) in triples
+    @test combine(a, combine(b, c)) == combine(combine(a, b), c)
+    if combine(a, b) == combine(a, c)
+      @test b == c
+    end
+    if combine(a, c) == combine(b, c)
+      @test a == b
     end
   end
 
-  #TODO: What is the property needed in general for this function?
-  #The property tested here for the int version is too strong and does not hold.
+  for elt in test_elts
+    for x in test_indivs
+      @test append(elt, x) == combine(elt, Index.base(Vector{Int}, x))
+    end
+  end
+
 end
 
-# I still don't understand a spec for base outside of the definitions -- jkoppel
-# No tests for Paired/pair, because they should not be exported
+# Not testing pair/Paired because they will be deleted
 
 end
