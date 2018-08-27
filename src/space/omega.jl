35ae3bce87324b28fb192467047980913f1cf565
James Koppel <jamesbkoppel@yahoo.com>
2018-08-27 18:18:13 -0400

    Comment on the Space interface

--- src/space/omega.jl
+++ src/space/omega.jl
@@ -6,6 +6,10 @@ abstract type ΩBase{I} <: Ω{I} end
 
 idtype(::Type{OT}) where {I, OT <: Ω{I}} = I
 
+# Must implement:
+# * Base.isempty
+# * Base.merge! ( -- currently unused)
+# ? others
 function resolve end
 
 "Linearize ω into flat vector"
