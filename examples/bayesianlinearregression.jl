3fdee0ac70e9a57779d27374bf17fe61d009501b
Zenna Tavares <zennatavares@gmail.com>
2021-02-25 02:32:05 -0500

    ntoebook

--- examples/bayesianlinearregression.jl
+++ examples/bayesianlinearregression.jl
@@ -16,9 +16,6 @@ using Random: MersenneTwister
 # ╔═╡ a44b4d1c-76cf-11eb-2cae-69ac6a56ca5a
 using Distributions
 
-# ╔═╡ 471c4b36-7734-11eb-0318-1fad66cf283c
-using Pkg; Pkg.status()
-
 # ╔═╡ 6b2fe98e-76cf-11eb-39f8-9ff52d23e688
 md"# Bayesian Linear Regression"
 
@@ -33,7 +30,7 @@ First, let's create some fake data"""
 rng = MersenneTwister(0);
 
 # ╔═╡ d10a9c5a-76ce-11eb-0ece-bd1b6e946fe0
-N = 1000;
+N = 100;
 
 # ╔═╡ d83b52d0-76ce-11eb-0d3f-d1ddd64d6e08
 xs = rand(rng, N)
@@ -60,31 +57,19 @@ UnicodePlots.scatterplot(xs, ys)
 md"## Probabilistic Model"
 
 # ╔═╡ 79cd6e26-76cf-11eb-1930-399093becba8
-M = 1 ~ Normal(0, 1) # FIXME: Do I want to ensure that 1 and 2 do not clash with 1 and 2 from other files?
+M = 1 ~ Normal(0, 1);
 
 # ╔═╡ a9765e1c-76cf-11eb-3495-79aa95e21afd
-C = 2 ~ Normal(0, 1)
+C = 2 ~ Normal(0, 1);
 
 # ╔═╡ b2cc4db2-76cf-11eb-390d-f151ff4b939d
-plate(i, ω) = linear_model(xs[i], M(ω), C(ω))# + (i, 1)# ~ Normal(0, 0.1) #FIXME: I'm not sure that (i, 1) is yet supported
+plate(i, ω) = linear_model(xs[i], M(ω), C(ω)) + ((i, 1) ~ Normal(0, 0.1))(ω);
 
-# ╔═╡ 97a07dd6-7728-11eb-0ca5-1f9181ebfdd3
-plate2(i, ω) = (i ~ Normal(0, 0.1))(ω)
-
-# ╔═╡ 6df94c4a-76d0-11eb-1d4a-1db3ad148b40
+# ╔═╡ 3fea6676-7735-11eb-3947-b77b2def96dc
 Y = (1:N) .~ plate  # FIXME: This is not correct notation
 
-# ╔═╡ 9694c020-7726-11eb-11fd-e97adf0fc518
-@which 1 ~ plate
-
-# ╔═╡ 50b2d750-7734-11eb-3f73-d556d625af70
-
-
-# ╔═╡ b2d578e8-7725-11eb-2903-8bea9e885ba3
-randsample(Y[1])
-
 # ╔═╡ 98baf47a-7724-11eb-2cd8-c36d03a11db2
-obs(w) = [linear_model(xs[i], M(w), C(w)) + Y[i](w) for i = 1:length(Y)]
+obs(ω) = [y(ω) for y in Y]
 
 # ╔═╡ fa8b5cc2-7728-11eb-24b3-5db1bd7d0255
 UnicodePlots.scatterplot(xs, randsample(obs))
@@ -122,12 +107,7 @@ UnicodePlots.scatterplot(xs, samples)
 # ╠═79cd6e26-76cf-11eb-1930-399093becba8
 # ╠═a9765e1c-76cf-11eb-3495-79aa95e21afd
 # ╠═b2cc4db2-76cf-11eb-390d-f151ff4b939d
-# ╠═97a07dd6-7728-11eb-0ca5-1f9181ebfdd3
-# ╠═6df94c4a-76d0-11eb-1d4a-1db3ad148b40
-# ╠═9694c020-7726-11eb-11fd-e97adf0fc518
-# ╠═471c4b36-7734-11eb-0318-1fad66cf283c
-# ╠═50b2d750-7734-11eb-3f73-d556d625af70
-# ╠═b2d578e8-7725-11eb-2903-8bea9e885ba3
+# ╠═3fea6676-7735-11eb-3947-b77b2def96dc
 # ╠═98baf47a-7724-11eb-2cd8-c36d03a11db2
 # ╠═fa8b5cc2-7728-11eb-24b3-5db1bd7d0255
 # ╠═c32b2f96-76d0-11eb-3f9c-e33cc3a1a7e7
