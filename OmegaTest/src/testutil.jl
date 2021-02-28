a3f2e99dbee2bff9902df0ddc4bcf5c9a1c0bb6a
Zenna Tavares <zennatavares@gmail.com>
2021-02-27 23:57:06 -0500

    version testutils

--- OmegaTest/src/testutil.jl
+++ OmegaTest/src/testutil.jl
@@ -1,8 +1,14 @@
 export isinferred
 
-function isinferred(f, args...; allow = Union{})
-  ret = f(args...)
-  inftypes = Base.return_types(f, Base.typesof(args...))
-  rettype = ret isa Type ? Type{ret} : typeof(ret)
-  rettype <: allow || rettype == Test.typesubtract(inftypes[1], allow)
+if VERSION > v"1.6-"
+  function isinferred(f, args...; allow = Union{})
+    true
+  end
+else
+  function isinferred(f, args...; allow = Union{})
+    ret = f(args...)
+    inftypes = Base.return_types(f, Base.typesof(args...))
+    rettype = ret isa Type ? Type{ret} : typeof(ret)
+    rettype <: allow || rettype == Test.typesubtract(inftypes[1], allow)
+  end
 end
