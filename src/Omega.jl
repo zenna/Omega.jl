d937cf10500e99083105c9ab912ee1d77a638659
Zenna Tavares <zennatavares@gmail.com>
2021-11-11 17:47:14 -0500

    more tests running

--- src/Omega.jl
+++ src/Omega.jl
@@ -6,11 +6,8 @@ using Reexport
 @reexport using OmegaCore
 @reexport using SoftPredicates
 
-include("omegadistributions.jl")     # Distributions.jl Distributions
-@reexport using .OmegaDistributions
-
-include("OmegaSoftPredicates.jl")
-@reexport using .OmegaSoftPredicates
+@reexport using OmegaDistributions
+@reexport using OmegaSoftPredicates
 
 @reexport using OmegaMH
 @reexport using ReplicaExchange
