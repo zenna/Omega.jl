5da7753c1aeb44f4751c4a76cf13f54ba7eebb08
Zenna Tavares <zennatavares@gmail.com>
2021-11-11 23:39:21 -0500

    Fix circular dep

--- InferenceBase/src/auto.jl
+++ InferenceBase/src/auto.jl
@@ -1,11 +1,10 @@
-
 export auto_init, auto_logenergy, logposterior
 import OmegaCore
 
-# FIXME: Remoev soft predicates from here, but how?!?
-using SoftPredicates
+# # FIXME: Remoev soft predicates from here, but how?!?
+# using SoftPredicates
 
-"Randomly initialize ω"
+"Randomly initialize `ω::ΩT`"
 function auto_init(rng, x, ΩT)
   ω = ΩT()
   ω_ = OmegaCore.tagignorecondition(OmegaCore.tagrng(ω, rng))
@@ -13,21 +12,22 @@ function auto_init(rng, x, ΩT)
   ω
 end 
 
-"Exo prior soft constraints"
-function auto_logenergy(x)
+"Exo prior soft constraints.  `T` is type of energy"
+function auto_logenergy(x, ::Type{T}) where T
   function logenergyvar_(ω)
-    ϵ = OmegaCore.condvarapply(x, ω, SoftPredicates.DualSoftBool{Float64})
+    ϵ = OmegaCore.condvarapply(x, ω, T) # need this in here for type stability - Julia limitation
     ℓ = OmegaCore.logenergyexo(ω)
     SoftPredicates.logerr(ϵ) + ℓ
   end
 end
 
-le(x::Real) = x
-le(x::SoftPredicates.AbstractSoftBool) = SoftPredicates.logerr(x)
+# le(x::Real) = x
+# le(x::SoftPredicates.AbstractSoftBool) = SoftPredicates.logerr(x)
 
+"Random variable that computes `logposterior(ω)` given `loglikelihood`"
 function logposterior(likelihood)
   function logpost_(ω)
-    ϵ = le(likelihood(ω))
+    ϵ = likelihood(ω)
     ℓ = OmegaCore.logenergyexo(ω)
     ϵ + ℓ
   end
