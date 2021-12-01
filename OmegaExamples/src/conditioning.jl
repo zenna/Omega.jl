6bc6af5d2d25baef5a9d17c14e1c3bc283a5e21e
archanarw <47085068+archanarw@users.noreply.github.com>
2021-12-02 00:05:02 +0530

    Add ch 2

--- OmegaExamples/src/conditioning.jl
+++ OmegaExamples/src/conditioning.jl
@@ -9,6 +9,7 @@ begin
     import Pkg
     # activate the shared project environment
     Pkg.activate(Base.current_project())
+<<<<<<< HEAD
     using Omega, Distributions, UnicodePlots, OmegaExamples
 end
 
@@ -17,6 +18,16 @@ md"# Cognition and conditioning"
 
 # ╔═╡ 877ff248-596c-4755-8085-281d1752ec5b
 md"We have built up a tool set for constructing probabilistic generative models. These can represent knowledge about causal processes in the world: running one of these programs generates a particular outcome by sampling a “history” for that outcome. However, the power of a causal model lies in the flexible ways it can be used to reason about the world. In the last chapter we used generative models to reason about outcomes from initial conditions. Generative models also enable reasoning in other ways. For instance, if we have a generative model in which $X$ is the output of a process that depends on $Y$ (say `X = cool_function(Y)`) we may ask: “Assuming I have observed a certain $X$, what must $Y$ have been?” That is we can reason backward from outcomes to initial conditions. More generally, we can make hypothetical assumptions and reason about the generative history: “assuming something, how did the generative model run?” In this section, we describe how a wide variety of such hypothetical inferences can be made from a single generative model by conditioning the model on an assumed or observed fact."
+=======
+    using Omega, Distributions, UnicodePlots
+end
+
+# ╔═╡ 711c87ea-9ec0-4957-bc00-641e2dd6eaec
+md"## Cognition and conditioning"
+
+# ╔═╡ 877ff248-596c-4755-8085-281d1752ec5b
+md"We have built up a tool set for constructing probabilistic generative models. These can represent knowledge about causal processes in the world: running one of these programs generates a particular outcome by sampling a “history” for that outcome. However, the power of a causal model lies in the flexible ways it can be used to reason about the world. In the last chapter we ran generative models forward to reason about outcomes from initial conditions. Generative models also enable reasoning in other ways. For instance, if we have a generative model in which X is the output of a process that depends on Y (say `X = coolFunction(Y)`) we may ask: “assuming I have observed a certain X, what must Y have been?” That is we can reason backward from outcomes to initial conditions. More generally, we can make hypothetical assumptions and reason about the generative history: “assuming something, how did the generative model run?” In this section we describe how a wide variety of such hypothetical inferences can be made from a single generative model by conditioning the model on an assumed or observed fact."
+>>>>>>> dd838e4 (Add ch 2)
 
 # ╔═╡ a0d93e64-92ca-4d32-905f-b99e5f3780d5
 md"Much of cognition can be understood in terms of conditional inference. In its most basic form, causal attribution is conditional inference: given some observed effects, what were the likely causes? Predictions are conditional inferences in the opposite direction: given that I have observed some cause, what are its likely effects? These inferences can be described by conditioning a probabilistic program that expresses a causal model. The acquisition of that causal model, or learning, is also conditional inference at a higher level of abstraction: given our general knowledge of how causal relations operate in the world, and some observed events in which candidate causes and effects co-occur in various ways, what specific causal relations are likely to hold between these observed variables?"
@@ -31,6 +42,7 @@ md"Parallel problems of conditional inference arise in visual perception, social
 md"When interacting with other people, we observe their actions, which result from a planning process, and often want to guess their desires, beliefs, emotions, or future actions. Planning can be modeled as a program that takes as input an agent’s mental states (beliefs, desires, etc.) and produces action sequences—for a rational agent, these will be actions that are likely to produce the agent’s desired states reliably and efficiently. A rational agent can plan their actions by conditional inference to infer what steps would be most likely to achieve their desired state. Action understanding, or interpreting an agent’s observed behavior, can be expressed as conditioning a planning program (a “theory of mind”) on observed actions to infer the mental states that most likely gave rise to those actions, and to predict how the agent is likely to act in the future."
 
 # ╔═╡ 60be7683-0b4c-467b-ba72-4d687bff9a1b
+<<<<<<< HEAD
 md"# Hypothetical Reasoning"
 
 # ╔═╡ bcfd08bc-91f6-4745-aeb0-9e7ecd218388
@@ -457,6 +469,12 @@ viz(randsample(TB_cond_, 1000))
 
 # ╔═╡ d6d61f0f-9ec7-4200-bdd6-7f3d3ee99d55
 md"Under this model, a patient with coughing, chest pain and shortness of breath is likely to have either lung cancer or TB. Modify the above code to see how these conditional inferences shift if you also know that the patient smokes or works in a hospital (where they could be exposed to various infections, including many worse infections than the typical person encounters). More generally, the causal structure of knowledge representation in a probabilistic program allows us to model intuitive theories that can grow in complexity continually over a lifetime, adding new knowledge without bound."
+=======
+md"## Hypothetical Reasoning"
+
+# ╔═╡ bcfd08bc-91f6-4745-aeb0-9e7ecd218388
+
+>>>>>>> dd838e4 (Add ch 2)
 
 # ╔═╡ Cell order:
 # ╠═f532f3b0-5293-11ec-104c-dba9836ec3d1
@@ -467,6 +485,7 @@ md"Under this model, a patient with coughing, chest pain and shortness of breath
 # ╟─59f09e31-aa76-4b73-b841-e6192841060e
 # ╟─bbbac3fa-3708-4d4e-87e1-01143a1b3eb0
 # ╟─60be7683-0b4c-467b-ba72-4d687bff9a1b
+<<<<<<< HEAD
 # ╟─bcfd08bc-91f6-4745-aeb0-9e7ecd218388
 # ╟─70d10737-6ec4-43fc-822d-a7db70fce4d0
 # ╠═c794ee68-86a0-49f8-9054-2f5efadd053d
@@ -569,3 +588,6 @@ md"Under this model, a patient with coughing, chest pain and shortness of breath
 # ╠═3fee9f6a-87b9-4097-8e66-42aab40f9e17
 # ╠═960755a2-138d-45a0-a0d7-872208e36023
 # ╟─d6d61f0f-9ec7-4200-bdd6-7f3d3ee99d55
+=======
+# ╠═bcfd08bc-91f6-4745-aeb0-9e7ecd218388
+>>>>>>> dd838e4 (Add ch 2)
