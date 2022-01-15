047ae45b56dac7d42a6cd6a40e9484f320d006ab
Zenna Tavares <zennatavares@gmail.com>
2022-01-15 17:35:24 -0500

    add license and uintid

--- OmegaCore/src/space/lazyomega.jl
+++ OmegaCore/src/space/lazyomega.jl
@@ -35,18 +35,6 @@ Basis.idtype(::Dict{T, V}) where {T, V} = T
 Basis.idtype(ω::LazyΩ{TAGS, Dict{T, V}}) where {TAGS, T, V} = T
 ids(ω::LazyΩ) = keys(ω.data)
 
-# ## Subspace
-# Basis.proj(ω::LazyΩ{TAGS, T}, ss) where {TAGS, T} = 
-#   LazyΩ{TAGS, T}(ω.data, ω.tags, append(ω.subspace, ss))
-
-#   # Basis.appendproj(ω::LazyΩ{TAGS, T}, ss::S) where {TAGS, T} = 
-# #   LazyΩ{TAGS, T}(ω.data, ω.tags, append(ss, ω.subspace))
-
-# Basis.updatesubspace(ω::LazyΩ{TAGS, T}, ss) where {TAGS, T} = 
-#   LazyΩ{TAGS, T}(ω.data, ω.tags, ss)
-
-# split(ω::LazyΩ) = ((sₗ, sᵣ) = split(subspace(ω)); (updatesubspace(ω, sₗ), updatesubspace(ω, sᵣ)))
-
 ## Tags
 replacetags(ω::LazyΩ, tags) = LazyΩ(ω.data, tags, ω.subspace)
 traits(::Type{LazyΩ{TAGS, T}}) where {TAGS, T} = traits(TAGS)
@@ -67,27 +55,6 @@ function Var.recurse(primrv::Var.PrimRandVar, ω::LazyΩ)
   end::eltype(newexo.class)
 end
 
-# function (exo::Var.PrimRandVar)(ω::LazyΩ)
-#   # idwhat = 2
-#   # @show exo.id
-#   # @show ω.subspace
-#   # @show 
-#   # Mix subspace with index
-#   newid = append(ω.subspace, exo.id)
-#   newexo = Var.Member(newid, exo.class)
-#   # @assert false
-#   # exo = exo
-#   result = get(ω.data, newexo, 0)
-#   if result === 0
-#     ω.data[newexo] = rand(rng(ω), newexo.class)
-#   else
-#     result
-#   end::eltype(newexo.class)
-# end
-
-
-
-
 ## Updating interface
 import ..Util:update
 export update, update!
@@ -104,15 +71,3 @@ function Base.show(io::IO, m::MIME"text/plain", ω::LazyΩ)
   println("\ndata:")
   show(io, m, ω.data)
 end
-
-# function resolve(dist, id, ω::LazyΩ)
-#   id_ = convertid(idtype(ω), id)
-#   # id_ = id
-#   if haskey(ω.data,  id_)
-#     d, val = ω.data[id_]
-#   else
-#     ω.data[id_] = (dist, rand(rng(ω), dist))
-#     d, val = ω.data[id_]
-#   end
-#   val::eltype(dist)   
-# end
