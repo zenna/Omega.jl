6cef56c737183ae6e226fe66b37183488f8f3246
Zenna Tavares <zennatavares@gmail.com>
2023-02-15 14:54:10 -0500

    rename test logpff

--- OmegaCore/test/logenergy.jl
+++ OmegaCore/test/logenergy.jl
@@ -27,40 +27,40 @@ import OmegaCore: propagate!
 #   ω = complete!(model, defω())
 # end
 
-function test_ble()
-  rng = MersenneTwister(0);
-  N = 100;
-  xs = rand(rng, N)
-  m = 0.8
-  c = 1.2
-  linear_model(x, m, c) = m * x + c
-  obs_model(x) = linear_model(x, m, c) + randn(rng) * 0.1;
-  ys = obs_model.(xs);
-  M = 1 ~ Normal(0, 1);
-  C = 2 ~ Normal(0, 1);
+# function test_ble()
+#   rng = MersenneTwister(0);
+#   N = 100;
+#   xs = rand(rng, N)
+#   m = 0.8
+#   c = 1.2
+#   linear_model(x, m, c) = m * x + c
+#   obs_model(x) = linear_model(x, m, c) + randn(rng) * 0.1;
+#   ys = obs_model.(xs);
+#   M = 1 ~ Normal(0, 1);
+#   C = 2 ~ Normal(0, 1);
 
-  # FIXME: Can't define struct here
-  # Alt? 
-  struct ϵ end
+#   # FIXME: Can't define struct here
+#   # Alt? 
+#   struct ϵ end
 
-  Y_class(i, ω) = linear_model(xs[i], M(ω), C(ω)) + (ϵ ∘ i ~ Normal(0, 0.1))(ω);
+#   Y_class(i, ω) = linear_model(xs[i], M(ω), C(ω)) + (ϵ ∘ i ~ Normal(0, 0.1))(ω);
 
-  Y⃗ = Mv(1:N, Y_class)
-  evidence = pw(==, Y⃗, ys)
-  joint_posterior = @joint(M, C) |ᶜ evidence
+#   Y⃗ = Mv(1:N, Y_class)
+#   evidence = pw(==, Y⃗, ys)
+#   joint_posterior = @joint(M, C) |ᶜ evidence
 
-  function propagate!(ω, ::typeof(Y⃗), Y⃗_)
-    for y_ in Y⃗_
-      propagate!(ω, Y_class, i, y_) # FIXME: no i defined and unclear how we'd get it
-    end
-  end
-  function propagate!(ω, ::typeof(Y_class), i, y_)
-    ϵ_ =  y_ -linear_model(xs[i], M(ω), C(ω))
-    # FIXME: still need to propagate
-  end
+#   function propagate!(ω, ::typeof(Y⃗), Y⃗_)
+#     for y_ in Y⃗_
+#       propagate!(ω, Y_class, i, y_) # FIXME: no i defined and unclear how we'd get it
+#     end
+#   end
+#   function propagate!(ω, ::typeof(Y_class), i, y_)
+#     ϵ_ =  y_ -linear_model(xs[i], M(ω), C(ω))
+#     # FIXME: still need to propagate
+#   end
 
-  complete!(joint_posterior, defω())
-end
+#   complete!(joint_posterior, defω())
+# end
 
 function test_logpdf_cond()
   μ = 1 ~ Normal(0, 1)
